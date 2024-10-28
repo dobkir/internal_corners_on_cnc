96e13a70b8f6118265416c7e73217be81da19e44
dobkir <dobkir@gmail.com>
2024-10-28 18:37:58 +0300

    refactor: output of numerical values ​​as rational numbers

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const WORKPIECE_THICKNESS = 22; // толщина заготовки
 const PARALLAX = 6;  // ширина фрезеровки по пласти
 const DEPTH = 6;  // глубина фрезеровки по Z
 const SAFE_HEIGHT = 20;  // безопасная высота по Z
@@ -8,44 +9,55 @@ const X1 = null;
 const X2 = null;
 const Y1 = null;
 const Y2 = null;
+const Z = WORKPIECE_THICKNESS - DEPTH;
 
-function getSafeZ(z) {
-    const safeZ = (z + DEPTH + SAFE_HEIGHT).toFixed(2);
+function getSafeZ(Z) {
+    const safeZ = (Z + DEPTH + SAFE_HEIGHT).toFixed(2);
     return safeZ;
 }
 
-function getInitZ(z) {
-    const initZ = +(z - ALLOWANCE).toFixed(2);
+function getInitZ(Z) {
+    const initZ = +(Z - ALLOWANCE).toFixed(2);
     return initZ;
 }
 
-function getFinalZ(z) {
-    const finalZ = +(getInitZ(z) + DEPTH).toFixed(2);
+function getFinalZ(Z) {
+    const finalZ = +(getInitZ(Z) + DEPTH).toFixed(2);
     return finalZ;
 }
 
-function getFinalDot(z) {
-    const finalDot = +(getInitZ(z) + DEPTH - ALLOWANCE).toFixed(2);
+function getFinalDot(Z) {
+    const finalDot = +(getInitZ(Z) + DEPTH - ALLOWANCE).toFixed(2);
     return finalDot;
 }
 
+function negativeOffset(initialPosition) {
+    const displacedPosition = +(initialPosition - ALLOWANCE).toFixed(2);
+    return displacedPosition;
+}
+
+function positiveOffset(initialPosition) {
+    const displacedPosition = +(initialPosition + ALLOWANCE).toFixed(2);
+    return displacedPosition;
+}
+
 function getInitX1(X1) {
-    const initX1 = +(X1 - ALLOWANCE).toFixed(2);
+    const initX1 = negativeOffset(X1);
     return initX1;
 }
 
 function getInitX2(X2) {
-    const initX2 = +(X2 + ALLOWANCE).toFixed(2);
+    const initX2 = positiveOffset(X2);
     return initX2;
 }
 
 function getInitY1(Y1) {
-    const initY1 = +(Y1 - ALLOWANCE).toFixed(2);
+    const initY1 = negativeOffset(Y1);
     return initY1;
 }
 
 function getInitY2(Y2) {
-    const initY2 = +(Y2 + ALLOWANCE).toFixed(2);
+    const initY2 = positiveOffset(Y2);
     return initY2;
 }
 
@@ -69,7 +81,7 @@ function getFinalY2(Y2) {
     return finalY2;
 }
 
-function getCode(X1, Y1, X2, Y2, z) {
+function getCode(X1, Y1, X2, Y2, Z) {
     const workpiecePoints = ['T1', 'T2', 'T3', 'T4'];
     const arrX = [X1, X1, X2, X2];
     const arrY = [Y1, Y2, Y2, Y1];
@@ -78,35 +90,38 @@ function getCode(X1, Y1, X2, Y2, z) {
         const X = arrX[index];
         const Y = arrY[index];
 
-        const initX = (X) => {
+        const initX = () => {
             const findInitX = (X === X1) ? getInitX1(X) : getInitX2(X);
-            return findInitX;
+            return Intl.NumberFormat("en", { style: "decimal", minimumFractionDigits: 2 }).format(findInitX);
         };
-        const initY = (Y) => {
+        const initY = () => {
             const findInitY = (Y === Y1) ? getInitY1(Y) : getInitY2(Y);
-            return findInitY;
+            return Intl.NumberFormat("en", { style: "decimal", minimumFractionDigits: 2 }).format(findInitY);
         };
-        const finalX = (X) => {
+        const finalX = () => {
             const findFinalX = (X === X1) ? getFinalX1(X) : getFinalX2(X);
-            return findFinalX;
+            return Intl.NumberFormat("en", { style: "decimal", minimumFractionDigits: 2 }).format(findFinalX);
         };
-        const finalY = (Y) => {
+        const finalY = () => {
             const findFinalY = (Y === Y1) ? getFinalY1(Y) : getFinalY2(Y);
-            return findFinalY;
+            return Intl.NumberFormat("en", { style: "decimal", minimumFractionDigits: 2 }).format(findFinalY);
         };
 
-        const safeZ = getSafeZ(z);
-        const initZ = getInitZ(z);
-        const finalZ = getFinalZ(z);
-        const finalDot = getFinalDot(z);
+        const bazeX = Intl.NumberFormat("en", { style: "decimal", minimumFractionDigits: 2 }).format(X);
+        const bazeY = Intl.NumberFormat("en", { style: "decimal", minimumFractionDigits: 2 }).format(Y);
+        const bazeZ = Intl.NumberFormat("en", { style: "decimal", minimumFractionDigits: 2 }).format(Z);
+        const safeZ = Intl.NumberFormat("en", { style: "decimal", minimumFractionDigits: 2 }).format(getSafeZ(Z));
+        const initZ = Intl.NumberFormat("en", { style: "decimal", minimumFractionDigits: 2 }).format(getInitZ(Z));
+        const finalZ = Intl.NumberFormat("en", { style: "decimal", minimumFractionDigits: 2 }).format(getFinalZ(Z));
+        const finalDot = Intl.NumberFormat("en", { style: "decimal", minimumFractionDigits: 2 }).format(getFinalDot(Z));
 
         const outerString = `
         (Diagonal Line ${index + 1} (${point}))
-        G0X${X}Y${Y}Z${getSafeZ(z)}
-        G1Z${z}F${F}
-        G1X${X}Y${Y}
-        G1X${initX(X)}Y${initY(Y)}Z${initZ}
-        X${finalX(X)}Y${finalY(Y)}Z${finalZ}
+        G0X${bazeX}Y${bazeY}Z${getSafeZ(Z)}
+        G1Z${bazeZ}F${F}
+        G1X${bazeX}Y${bazeY}
+        G1X${initX()}Y${initY()}Z${initZ}
+        X${finalX()}Y${finalY()}Z${finalZ}
         Z${finalDot}
         Z${finalZ}
         G0Z${safeZ}
@@ -115,13 +130,8 @@ function getCode(X1, Y1, X2, Y2, z) {
 
         return outerString;
     })
-    // function getX (T) {
-    //     switch (T) {
-    //       case 'T1':
-    //         return 'X1';
-    //     }
-    // }
-    return stringMessage.join(' ');
+
+    return stringMessage.join('');
 }
 
 console.log(getCode(25, 60, 205, 689, 16));
@@ -168,137 +178,4 @@ Z21.8
 Z21.9
 G0Z42.00
 (End of the Diagonal Line 4)
-*/
-
-/*
-function getT1(lineNumber, x, y, z) {
-
-    const safeZ = getSafeZ(z);
-
-    const initX = +(x - ALLOWANCE).toFixed(2);
-    const initY = +(y - ALLOWANCE).toFixed(2);
-    const initZ = +(z - ALLOWANCE).toFixed(2);
-
-    const finalX = +(initX - PARALLAX).toFixed(2);
-    const finalY = +(initY - PARALLAX).toFixed(2);
-    const finalZ = +(initZ + DEPTH).toFixed(2);
-    const finalDot = +(initZ + DEPTH - ALLOWANCE).toFixed(2);
-
-    const stringMessageT1 = `
-    (Diagonal Line ${lineNumber})
-    G0X${x}Y${y}Z${safeZ}
-    G1Z${z}F${F}
-    G1X${x}Y${y}
-    G1X${initX}Y${initY}Z${initZ}
-    X${finalX}Y${finalY}Z${finalZ}
-    Z${finalDot}
-    Z${finalZ}
-    G0Z${safeZ}
-    (End of the Diagonal Line ${lineNumber})
-    `;
-
-    return stringMessageT1;
-}
-
-// (Diagonal Line 1)
-// G0X78.5Y113.5Z42.0
-// G1Z14.0F1200
-// G1X78.5Y113.5
-// G1X78.40Y113.40Z13.90
-// X64.40Y99.40Z21.90
-// Z21.80
-// Z21.90
-// (End of the Diagonal Line 1)
-
-function getT2(lineNumber, x, y, z) {
-
-    const safeZ = getSafeZ(z);
-
-    const initX = +(x - ALLOWANCE).toFixed(2);
-    const initY = +(y + ALLOWANCE).toFixed(2);
-    const initZ = +(z - ALLOWANCE).toFixed(2);
-
-    const finalX = +(initX - PARALLAX).toFixed(2);
-    const finalY = +(initY + PARALLAX).toFixed(2);
-    const finalZ = +(initZ + DEPTH).toFixed(2);
-    const finalDot = +(initZ + DEPTH - ALLOWANCE).toFixed(2);
-
-    const stringMessageT2 = `
-    (Diagonal Line ${lineNumber})
-    G0X${x}Y${y}Z${safeZ}
-    G1Z${z}F${F}
-    G1X${x}Y${y}
-    G1X${initX}Y${initY}Z${initZ}
-    X${finalX}Y${finalY}Z${finalZ}
-    Z${finalDot}
-    Z${finalZ}
-    G0Z${safeZ}
-    (End of the Diagonal Line ${lineNumber})
-    `;
-
-    return stringMessageT2;
-}
-
-function getT3(lineNumber, x, y, z) {
-
-    const safeZ = getSafeZ(z);
-
-    const initX = +(x + ALLOWANCE).toFixed(2);
-    const initY = +(y + ALLOWANCE).toFixed(2);
-    const initZ = +(z - ALLOWANCE).toFixed(2);
-
-    const finalX = +(initX + PARALLAX).toFixed(2);
-    const finalY = +(initY + PARALLAX).toFixed(2);
-    const finalZ = +(initZ + DEPTH).toFixed(2);
-    const finalDot = +(initZ + DEPTH - ALLOWANCE).toFixed(2);
-
-    const stringMessageT3 = `
-    (Diagonal Line ${lineNumber})
-    G0X${x}Y${y}Z${safeZ}
-    G1Z${z}F${F}
-    G1X${x}Y${y}
-    G1X${initX}Y${initY}Z${initZ}
-    X${finalX}Y${finalY}Z${finalZ}
-    Z${finalDot}
-    Z${finalZ}
-    G0Z${safeZ}
-    (End of the Diagonal Line ${lineNumber})
-    `;
-
-    return stringMessageT3;
-}
-
-function getT4(lineNumber, x, y, z) {
-
-    const safeZ = getSafeZ(z);
-
-    const initX = +(x + ALLOWANCE).toFixed(2);
-    const initY = +(y - ALLOWANCE).toFixed(2);
-    const initZ = +(z - ALLOWANCE).toFixed(2);
-
-    const finalX = +(initX + PARALLAX).toFixed(2);
-    const finalY = +(initY - PARALLAX).toFixed(2);
-    const finalZ = +(initZ + DEPTH).toFixed(2);
-    const finalDot = +(initZ + DEPTH - ALLOWANCE).toFixed(2);
-
-    const stringMessageT4 = `
-    (Diagonal Line ${lineNumber})
-    G0X${x}Y${y}Z${safeZ}
-    G1Z${z}F${F}
-    G1X${x}Y${y}
-    G1X${initX}Y${initY}Z${initZ}
-    X${finalX}Y${finalY}Z${finalZ}
-    Z${finalDot}
-    Z${finalZ}
-    G0Z${safeZ}
-    (End of the Diagonal Line ${lineNumber})
-    `;
-
-    return stringMessageT4;
-}
-
-console.log(getT1(1, 239, 2610, 16));
-console.log(getT2(2, 239, 2790, 16));
-console.log(getT3(3, 868, 2790, 16));
-console.log(getT4(4, 868, 2610, 16));
 */
